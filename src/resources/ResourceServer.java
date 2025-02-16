4e89755d5c8b5f0d0860fb95d89a673353468a95
Iswed <milanastasenko5@gmail.com>
2025-02-16 21:06:04 +0300

    change

--- src/resources/ResourceServer.java
+++ src/resources/ResourceServer.java
@@ -1,2 +1,19 @@
-package resources;public class ResourceServer {
+package resources;
+
+public class ResourceServer implements ResourceServerMBean{
+    private TestResource testResource = new TestResource();
+
+    public void setTestResource(TestResource resource) {
+        this.testResource = resource;
+    }
+
+    @Override
+    public String getName() {
+        return testResource.getName();
+    }
+
+    @Override
+    public int getAge() {
+        return testResource.getAge();
+    }
 }
--- src/resources/ResourceServlet.java
+++ src/resources/ResourceServlet.java
@@ -1,2 +1,38 @@
-package resources;public class ResourceServlet {
+package resources;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+@WebServlet("/resources")
+public class ResourceServlet extends HttpServlet {
+    private final ResourceServer resourceServer;
+
+    public ResourceServlet(ResourceServer resourceServer) {
+        this.resourceServer = resourceServer;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getParameter("path");
+        if (path == null || path.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        File file = new File(path);
+        if (!file.exists()) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        XmlMapper xmlMapper = new XmlMapper();
+        TestResource resource = xmlMapper.readValue(file, TestResource.class);
+        resourceServer.setTestResource(resource);
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
 }
